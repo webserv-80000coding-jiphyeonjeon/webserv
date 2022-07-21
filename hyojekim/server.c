78454d6d1678d174f04f0e0f69d4cc2a72fc1b52
hyojekim <42.4.hyojekim@gmail.com>
2022-07-21 14:55:27 +0900

    feat: :sparkles: selet() 함수 적용 연습
    server 1개에 여러 client를 연결하는 multiplexing 적용 연습

--- hyojekim/server.c
+++ hyojekim/server.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -29,6 +30,10 @@ int main(int argc, char* argv[]) {
   struct sockaddr_in client_addr;
   socklen_t          client_addr_size;
 
+  struct timeval timeout;
+  fd_set         reads, cpy_reads;
+  int            fd_max, fd_num, i;
+
   server_socket = socket(PF_INET, SOCK_STREAM, 0);
   if (server_socket == -1)
     error_handling(RED "socket() error" END);
@@ -44,20 +49,47 @@ int main(int argc, char* argv[]) {
   if (listen(server_socket, 5) == -1)
     error_handling(RED "listen() error" END);
 
-  client_addr_size = sizeof(client_addr);
-  client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_size);
-  if (client_socket == -1)
-    error_handling("accept() error");
-  printf(GRN "Success to connect client" END);
-  printf("\n");
+  FD_ZERO(&reads);
+  FD_SET(server_socket, &reads);
+  fd_max = server_socket;
 
-  while ((str_len = read(client_socket, message, BUFFER_SIZE)) != 0) {
-    write(client_socket, message, str_len);
-    write(1, message, str_len);
+  while (1) {
+    cpy_reads = reads;
+    timeout.tv_sec = 5;
+    timeout.tv_usec = 0;
+
+    if ((fd_num = select(fd_max + 1, &cpy_reads, 0, 0, &timeout)) == -1)
+      break;
+
+    if (fd_num == 0)
+      continue;
+
+    for (i = 0; i <= fd_max; i++) {
+      if (FD_ISSET(i, &cpy_reads)) {
+        if (i == server_socket) {
+          client_addr_size = sizeof(client_addr);
+          client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_size);
+          if (client_socket == -1)
+            error_handling("accept() error");
+          FD_SET(client_socket, &reads);
+          if (fd_max < client_socket)
+            fd_max = client_socket;
+          printf(GRN "Accepted client: %d" END "\n", client_socket);
+        } else {
+          str_len = read(i, message, BUFFER_SIZE);
+          if (str_len == 0) {
+            FD_CLR(i, &reads);
+            close(i);
+            printf(RED "Client %d is closed" END "\n", i);
+          } else {
+            write(i, message, str_len);
+            write(1, message, str_len);
+          }
+        }
+      }
+    }
   }
 
   close(server_socket);
-  close(client_socket);
-
   return 0;
 }
