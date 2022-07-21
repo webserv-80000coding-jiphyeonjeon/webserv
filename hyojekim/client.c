b072e0a4135c1de7b3f0325e2d29bafc529fee7c
hyojekim <42.4.hyojekim@gmail.com>
2022-07-21 14:35:10 +0900

    feat: :sparkles: 가장 간단한 echo server 만들기
    c를 기반으로 하는 echo server 연습
    - 소켓프로그래밍 흐름 복습하기
    - server 1개와 client 1개만 이어지는 상황

--- hyojekim/client.c
+++ hyojekim/client.c
@@ -3,7 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
+
+#include "color.h"
+
+#define BUFFER_SIZE 1024
 
 void error_handling(char* message) {
   fputs(message, stderr);
@@ -14,29 +19,38 @@ void error_handling(char* message) {
 int main(int argc, char* argv[]) {
   int                client_socket;
   struct sockaddr_in server_addr;
-  char               message[1024] = {
-                    0x00,
-  };
+  char               message[BUFFER_SIZE];
+  int                str_len;
+
+  if (argc != 3)
+    error_handling(RED "Usage: ./client <ip> <port>" END);
 
   client_socket = socket(PF_INET, SOCK_STREAM, 0);
-  if (client_socket == -1) {
-    error_handling("socket() error");
-  }
+  if (client_socket == -1)
+    error_handling(RED "socket() error" END);
 
   memset(&server_addr, 0, sizeof(server_addr));
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = inet_addr(argv[1]);
   server_addr.sin_port = htons(atoi(argv[2]));
 
-  if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-    error_handling("connect() error");
-  }
-  printf("Success to connect server\n");
+  if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
+    error_handling(RED "connect() error" END);
 
-  if (read(client_socket, message, sizeof(message)) == -1) {
-    error_handling("read() error");
+  printf(GRN "Success to connect server\n" END);
+
+  while (1) {
+    fputs("Input message (q to quit): ", stdout);
+    fgets(message, BUFFER_SIZE, stdin);
+
+    if (!strcmp(message, "q\n"))
+      break;
+    write(client_socket, message, strlen(message));
+
+    str_len = read(client_socket, message, BUFFER_SIZE - 1);
+    message[str_len] = 0;
+    printf("Message from server: %s\n", message);
   }
-  printf("%s", message);
 
   close(client_socket);
   return 0;
--- hyojekim/server.c
+++ hyojekim/server.c
@@ -3,7 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
+
+#include "color.h"
+
+#define BUFFER_SIZE 1024
 
 void error_handling(char* message) {
   fputs(message, stderr);
@@ -12,39 +17,44 @@ void error_handling(char* message) {
 }
 
 int main(int argc, char* argv[]) {
-  int server_socket;
-  int client_socket;
+  int  server_socket;
+  int  client_socket;
+  char message[BUFFER_SIZE];
+  int  str_len;
+
+  if (argc != 2)
+    error_handling(RED "Usage: ./server <port>" END);
 
   struct sockaddr_in server_addr;
   struct sockaddr_in client_addr;
   socklen_t          client_addr_size;
 
   server_socket = socket(PF_INET, SOCK_STREAM, 0);
-  if (server_socket == -1) {
-    error_handling("socket() error");
-  }
+  if (server_socket == -1)
+    error_handling(RED "socket() error" END);
 
   memset(&server_addr, 0, sizeof(server_addr));
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
   server_addr.sin_port = htons(atoi(argv[1]));
 
-  if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-    error_handling("bind() error");
-  }
+  if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
+    error_handling(RED "bind() error" END);
 
-  if (listen(server_socket, 5) == -1) {
-    error_handling("listen() error");
-  }
+  if (listen(server_socket, 5) == -1)
+    error_handling(RED "listen() error" END);
 
   client_addr_size = sizeof(client_addr);
   client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_size);
-  if (client_socket == -1) {
+  if (client_socket == -1)
     error_handling("accept() error");
-  }
+  printf(GRN "Success to connect client" END);
+  printf("\n");
 
-  char msg[] = "Hello this is server!\n";
-  write(client_socket, msg, sizeof(msg));
+  while ((str_len = read(client_socket, message, BUFFER_SIZE)) != 0) {
+    write(client_socket, message, str_len);
+    write(1, message, str_len);
+  }
 
   close(server_socket);
   close(client_socket);
