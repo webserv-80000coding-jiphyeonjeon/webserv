75a77fb7edcb9192502e1d5f82eca76743c9be19
hyojekim <42.4.hyojekim@gmail.com>
2022-07-31 13:43:58 +0900

    feat: :sparkles: select()를 활용한 echo server 구현 #6
    select()함수를 사용해 multiplexing I/O 구현
    - echo server 만들기

--- include/Server.hpp
+++ include/Server.hpp
@@ -8,23 +8,35 @@
 
 #include "Config.hpp"
 
+enum RecvState { kClosedClient = 0, kRecvError = -1, kRecvSuccess = 1 };
+enum SendState { kSendError = -1, kSendSuccess = 0 };
+
 class Server {
  public:
-  typedef ConfigServer::ListenType ListenType;
-  typedef int                      FdType;
+  typedef ConfigServer::ListenType      ListenType;
+  typedef int                           FdType;
+  typedef std::map<FdType, std::string> RequestType;
 
   Server(const ListenType& listen);
   ~Server();
 
-  void   initServer();
-  FdType getFd();
+  FdType                 getFd() const;
+  ConfigServer::PortType getPort() const;
+
+  void      initServer();
+  FdType    acceptClient();
+  RecvState recieveData(FdType client_socket);
+  SendState sendData(FdType client_socket);
+  void      closeClient(FdType client_socket);
 
  private:
   static const int kBacklog = 1024;
+  static const int kBufferSize = 1024;
 
   ListenType         listen_;
   int                socket_;
   struct sockaddr_in addr_;
+  RequestType        request_map_;
 
   void setAddr();
 };
