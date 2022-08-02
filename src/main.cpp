bc0ddd5c196cb35be1bd82307603216e0b432ec5
Devdha <france998811@gmail.com>
2022-08-02 21:01:15 +0900

    test: :test_tube: Request header 파싱 테스트

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,65 @@
 #include <iostream>
 #include <string>
 
-#include "Parser.hpp"
-#include "Webserv.hpp"
-#include "color.hpp"
+// #include "Parser.hpp"
+// #include "Webserv.hpp"
+// #include "color.hpp"
 
-int main(int argc, char* argv[]) {
-  if (argc > 2) {
-    std::cout << "Usage: " << argv[0] << " [config/*.conf]" << std::endl;
-    return 1;
-  }
+// int main(int argc, char* argv[]) {
+//   if (argc > 2) {
+//     std::cout << "Usage: " << argv[0] << " [config/*.conf]" << std::endl;
+//     return 1;
+//   }
 
-  // ************************************************************************ //
-  //                                 Parsing                                  //
-  // ************************************************************************ //
+//   // ************************************************************************
+//   //
+//   //                                 Parsing //
+//   // ************************************************************************
+//   //
 
-  Config      config;
-  std::string filename = (argc == 2) ? argv[1] : "./config/default.conf";
+//   Config      config;
+//   std::string filename = (argc == 2) ? argv[1] : "./config/default.conf";
 
+//   try {
+//     Parser parser(filename);
+//     parser.parse(config);
+//     config.printConfig();
+
+//   } catch (std::exception& e) {
+//     std::cerr << RED << e.what() << END << std::endl;
+//     return 1;
+//   }
+
+//   // ************************************************************************
+//   //
+//   //                              Server Setting //
+//   // ************************************************************************
+//   //
+
+//   try {
+//     Webserv webserv;
+//     webserv.initWebserv(config);
+//     webserv.runWebserv();
+//   } catch (std::exception& e) {
+//     std::cerr << RED << e.what() << END << std::endl;
+//     return 1;
+//   } catch (...) {
+//     std::cerr << "Unknown error" << std::endl;
+//     return 1;
+//   }
+// }
+
+#include "Request.hpp"
+#include "Utilities.hpp"
+
+int main() {
+  Request     req;
+  std::string msg =
+      "GET / HTTP/1.1\r\nHost: localhost:8080\r\nContent-length: 10\r\n\r\n";
   try {
-    Parser parser(filename);
-    parser.parse(config);
-    config.printConfig();
-
-  } catch (std::exception& e) {
-    std::cerr << RED << e.what() << END << std::endl;
-    return 1;
-  }
-
-  // ************************************************************************ //
-  //                              Server Setting                              //
-  // ************************************************************************ //
-
-  try {
-    Webserv webserv;
-    webserv.initWebserv(config);
-    webserv.runWebserv();
-  } catch (std::exception& e) {
-    std::cerr << RED << e.what() << END << std::endl;
-    return 1;
-  } catch (...) {
-    std::cerr << "Unknown error" << std::endl;
-    return 1;
+    req.parse(msg);
+    req.print();
+  } catch (Request::RequestException& e) {
+    std::cout << e.what() << " " << e.getStatusCode() << std::endl;
   }
 }
