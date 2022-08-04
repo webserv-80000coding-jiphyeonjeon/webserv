54487f2afd9bbafa0660d2e146c621d35bde72a4
Devdha <france998811@gmail.com>
2022-08-04 21:51:20 +0900

    feat: :sparkles: Response 클래스 정의 및 구현 #24

---
+++ include/Response.hpp
@@ -0,0 +1,43 @@
+#ifndef WEBSERV_RESPONSE_HPP
+#define WEBSERV_RESPONSE_HPP
+
+#include <map>
+#include <sstream>
+#include <string>
+
+class Response {
+ public:
+  typedef std::string                              MessageType;
+  typedef std::string                              VersionType;
+  typedef int                                      StatusCodeType;
+  typedef std::string                              StatusCodeMessageType;
+  typedef std::map<int, std::string>               StatusCodeMapType;
+  typedef std::string                              HeaderKeyType;
+  typedef std::string                              HeaderValueType;
+  typedef std::map<HeaderKeyType, HeaderValueType> HeaderMapType;
+  typedef std::string                              BodyType;
+
+  Response();
+  ~Response();
+
+  const MessageType& getMessage() const;
+
+  void setVersion(const VersionType& version);
+  void setStatusCode(const StatusCodeType& status_code);
+  void setHeader(const HeaderKeyType& key, const HeaderValueType& value);
+  void setBody(const BodyType& body);
+
+  void build();
+
+ private:
+  void initStatusCodeMap();
+
+  MessageType       message_;
+  VersionType       version_;
+  StatusCodeType    status_code_;
+  StatusCodeMapType status_code_map_;
+  HeaderMapType     header_map_;
+  BodyType          body_;
+};
+
+#endif
